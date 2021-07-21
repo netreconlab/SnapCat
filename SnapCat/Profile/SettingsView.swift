58a4663991d17318c6d05d229f95259d822ec200
Corey <coreyearleon@icloud.com>
2021-07-21 15:21:31 -0400

    Add markdown files to project (#6)
    * Add markdown files to project
    
    * Add logo

--- SnapCat/Profile/SettingsView.swift
+++ SnapCat/Profile/SettingsView.swift
@@ -11,9 +11,11 @@ import AuthenticationServices
 
 struct SettingsView: View {
     @ObservedObject var viewModel = SettingsViewModel()
+    @State private var tintColor = UIColor { $0.userInterfaceStyle == .light ?  #colorLiteral(red: 0, green: 0.2858072221, blue: 0.6897063851, alpha: 1) : #colorLiteral(red: 0.06253327429, green: 0.6597633362, blue: 0.8644603491, alpha: 1) }
     @Environment(\.presentationMode) var presentationMode
     var body: some View {
         VStack {
+            Spacer()
             if !User.apple.isLinked {
                 SignInWithAppleButton(.continue,
                                       onRequest: { (request) in
@@ -44,6 +46,25 @@ struct SettingsView: View {
             })
             .background(Color(.red))
             .cornerRadius(15)
+            Spacer()
+            if let link = URL(string: "https://www.cs.uky.edu/~baker/"),
+               let image = UIImage(named: "netrecon") {
+                HStack {
+                    Spacer()
+                    Link(destination: link, label: {
+                        VStack {
+                            Text("Developed by the")
+                                .foregroundColor(Color(tintColor))
+                            Text("University of Kentucky")
+                                .foregroundColor(Color(tintColor))
+                            Image(uiImage: image)
+                                .resizable()
+                                .frame(width: 100, height: 50, alignment: .center)
+                        }
+                    })
+                    Spacer()
+                }
+            }
         }.onAppear(perform: {
             if User.current == nil {
                 presentationMode.wrappedValue.dismiss()
