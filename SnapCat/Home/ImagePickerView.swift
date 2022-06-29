cc19733a097888a690c7a44eb2e88b7752ab8b92
kingjalen22 <jalen.collins@uky.edu>
2022-06-29 16:38:25 -0400

    fix: UIBugs

--- SnapCat/Home/ImagePickerView.swift
+++ SnapCat/Home/ImagePickerView.swift
@@ -40,10 +40,12 @@ struct ImagePickerView: UIViewControllerRepresentable {
         }
 
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            DispatchQueue.main.async {
             if let uiImage = info[.originalImage] as? UIImage {
-                parent.image = uiImage
+                self.parent.image = uiImage
             }
-            parent.presentationMode.wrappedValue.dismiss()
+                self.parent.presentationMode.wrappedValue.dismiss()
+        }
         }
     }
 }
